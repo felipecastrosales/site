3f507dc39160d537a54a491a1ed4ba1dabc51279
Felipe Sales <soufeliposales@gmail.com>
2022-01-14 21:35:37 -0300

    setting firebase hosting with 'news soon'

---
+++ lib/home/home_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Novidades em breve...'),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'home/home_page.dart';
+
 void main() {
   runApp(
     const MyApp(),
@@ -16,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(),
+      home: const HomePage(),
     );
   }
 }
