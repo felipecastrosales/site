738712442f4b248cb57d32753bda638067fd921f
Felipe Sales <soufeliposales@gmail.com>
2022-08-09 23:17:35 -0300

    adding ConstantsAPI and updating ContactRepository

---
+++ lib/data/constants/constants_api.dart
@@ -0,0 +1,8 @@
+class ConstantsAPI {
+  ConstantsAPI._();
+  static const baseUrl = 'https://api.emailjs.com/api/v1.0/email/send';
+  static const headers = {
+    'origin': 'http://localhost',
+    'Content-Type': 'application/json',
+  };
+}
--- lib/data/repositories/contact_repository_impl.dart
+++ lib/data/repositories/contact_repository_impl.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
+import 'package:site/data/constants/constants_api.dart';
 import 'package:site/data/keys/keys.dart';
 
 import 'contact_repository.dart';
@@ -14,18 +15,17 @@ class ContactRepositoryImpl implements ContactRepository {
     required String message,
     required String subject,
   }) async {
-    final url = Uri.parse('https://api.emailjs.com/api/v1.0/email/send');
-    // as this project is very simple and I wanted to make it available to the community, these private keys are simply in a 'keys' file and I added it to .gitignore.
+    final baseUrl = Uri.parse(ConstantsAPI.baseUrl);
+    // as this project is very simple and I wanted to make it available to the community, 
+    // these private keys are simply in a 'keys' file and I added it to .gitignore.
     // ! you can and should work it out better, because it's about security. !
     const serviceId = Keys.serviceId;
     const templateId = Keys.templateId;
     const userId = Keys.userId;
+
     final response = await http.post(
-      url,
-      headers: {
-        'origin': 'http://localhost',
-        'Content-Type': 'application/json',
-      },
+      baseUrl,
+      headers: ConstantsAPI.headers,
       body: json.encode(
         {
           'service_id': serviceId,
