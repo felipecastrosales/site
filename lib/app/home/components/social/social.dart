2dbe2fa89749bf2ca148df75f3e952f8c6f6f6f8
Felipe Sales <soufeliposales@gmail.com>
2022-08-31 21:27:59 -0300

    chore: setuping and following the Social flow

--- lib/app/home/components/social/social.dart
+++ lib/app/home/components/social/social.dart
@@ -1,63 +1,23 @@
 import 'package:flutter/material.dart';
 
-import 'package:site/app/core/app_customs/app_customs.dart';
-import 'package:site/app/widgets/body/body.dart';
-import 'package:site/app/widgets/dividers/dividers.dart';
-import 'package:site/app/widgets/section/section.dart';
+import 'package:site/app/core/responsive/breakpoints.dart';
 
-import 'components/components.dart';
+import 'social_mobile.dart';
+import 'social_web.dart';
 
 class Social extends StatelessWidget {
   const Social({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Positioned(
-          bottom: 0,
-          child: Image.asset(
-            AppImages.socialAbstract,
-            fit: BoxFit.cover,
-            filterQuality: FilterQuality.high,
-            width: MediaQuery.of(context).size.width,
-          ),
-        ),
-        Positioned.fill(
-          bottom: 0,
-          child: Align(
-            child: Image.asset(
-              AppImages.socialGradient,
-              fit: BoxFit.cover,
-              filterQuality: FilterQuality.high,
-              width: MediaQuery.of(context).size.width,
-            ),
-          ),
-        ),
-        Column(
-          children: const [
-            MobileBody(
-              children: [
-                SectionTitle(
-                  paddingTop: 50,
-                  paddingBottom: 20,
-                  title: 'Social Links',
-                ),
-                SectionText(
-                  paddingTop: 0,
-                  paddingBottom: 24,
-                  title: 'Me acompanhe nas minhas redes:',
-                ),
-                Center(
-                  child: SocialList(),
-                ),
-              ],
-            ),
-            SizedBox(height: 60),
-            SectionDivider(),
-          ],
-        ),
-      ],
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth < Breakpoints.social) {
+          return const SocialMobile();
+        } else {
+          return const SocialWeb();
+        }
+      },
     );
   }
 }
---
+++ lib/app/home/components/social/social_web.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+import 'package:site/app/core/app_customs/app_customs.dart';
+import 'package:site/app/widgets/body/body.dart';
+import 'package:site/app/widgets/dividers/dividers.dart';
+import 'package:site/app/widgets/section/section.dart';
+
+import 'components/components.dart';
+
+class SocialWeb extends StatelessWidget {
+  const SocialWeb({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Positioned(
+          bottom: 0,
+          child: Image.asset(
+            AppImages.socialAbstract,
+            fit: BoxFit.cover,
+            filterQuality: FilterQuality.high,
+            width: MediaQuery.of(context).size.width,
+          ),
+        ),
+        Positioned.fill(
+          bottom: 0,
+          child: Align(
+            child: Image.asset(
+              AppImages.socialGradient,
+              fit: BoxFit.cover,
+              filterQuality: FilterQuality.high,
+              width: MediaQuery.of(context).size.width,
+            ),
+          ),
+        ),
+        Column(
+          children: const [
+            MobileBody(
+              children: [
+                SectionTitle(
+                  paddingTop: 50,
+                  paddingBottom: 20,
+                  title: 'Social Links',
+                ),
+                SectionText(
+                  paddingTop: 0,
+                  paddingBottom: 24,
+                  title: 'Me acompanhe nas minhas redes:',
+                ),
+                Center(
+                  child: SocialList(),
+                ),
+              ],
+            ),
+            SizedBox(height: 60),
+            SectionDivider(),
+          ],
+        ),
+      ],
+    );
+  }
+}
