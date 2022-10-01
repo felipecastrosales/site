63538277a15d3580d6d54d6c7ee09de1613256d9
Felipe Sales <soufeliposales@gmail.com>
2022-09-30 21:12:58 -0300

    wip: update ImageUtils

--- lib/app/utils/image_utils.dart
+++ lib/app/utils/image_utils.dart
@@ -1,32 +1,31 @@
 import 'package:flutter/material.dart';
 
 class ImageUtils extends StatelessWidget {
-  const ImageUtils({
+  const ImageUtils(
+    this.image, {
     super.key,
-    required this.image,
-    required this.fit,
-    required this.width,
-    required this.height,
+    this.fit = BoxFit.cover,
     this.alignment = Alignment.center,
     this.filterQuality = FilterQuality.high,
+    this.height,
+    this.width,
   });
 
   final String image;
   final BoxFit fit;
-  final double width;
-  final double height;
   final Alignment alignment;
   final FilterQuality filterQuality;
+  final double? height, width;
 
   @override
   Widget build(BuildContext context) {
     return Image.asset(
       image,
       fit: fit,
-      height: height,
-      width: width,
       alignment: alignment,
       filterQuality: filterQuality,
+      height: height ?? MediaQuery.of(context).size.height,
+      width: width ?? MediaQuery.of(context).size.width,
     );
   }
 }
