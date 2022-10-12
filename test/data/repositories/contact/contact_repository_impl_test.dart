2af4c77e418ed61e6d52fea8af0893f2372cf20a
Felipe Sales <soufeliposales@gmail.com>
2022-10-12 17:50:48 -0300

    test: created ContactRepository unit test

---
+++ test/data/repositories/contact/contact_repository_impl_test.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:site/data/repositories/contact/contact.dart';
+
+void main() {
+  testWidgets('ContactRepositoryImpl', (tester) async {
+    var contactRepositoryImpl = ContactRepositoryImpl();
+
+    expect(
+      contactRepositoryImpl,
+      isNotNull,
+    );
+
+    await contactRepositoryImpl.sendMail(
+      name: 'felipecastrosales',
+      email: 'soufeliposales@gmail.com',
+      message: 'Hello, World!',
+      subject: 'Hello, World!',
+    );
+  });
+}
