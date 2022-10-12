c9b71483136581e1ae2b8c53dd4291ebd28657d1
Felipe Sales <soufeliposales@gmail.com>
2022-10-12 15:39:38 -0300

    wip: added Keys class on project and update gitignore

---
+++ lib/data/keys/keys.dart
@@ -0,0 +1,7 @@
+import 'keys_impl.dart';
+
+class Keys {
+  static const serviceId = KeysImpl.serviceId;
+  static const templateId = KeysImpl.templateId;
+  static const userId = KeysImpl.userId;
+}
