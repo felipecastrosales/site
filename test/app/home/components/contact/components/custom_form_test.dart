3fc9bd340c45ae55a13358116af12152bce57c2d
Felipe Sales <soufeliposales@gmail.com>
2022-10-14 19:35:55 -0300

    fix: adjust test with error

--- test/app/home/components/contact/components/custom_form_test.dart
+++ test/app/home/components/contact/components/custom_form_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:site/app/home/components/contact/components/components.dart';
-import 'package:site/app/utils/utils.dart';
 
 void main() {
   testWidgets('Should renders CustomForm', (tester) async {
@@ -16,10 +15,7 @@ void main() {
         (widget) =>
             widget is CustomTextFormField &&
             widget.hintText == 'Nome' &&
-            widget.prefixIcon == Icons.account_circle &&
-            widget.validator == ContactValitadors.name &&
-            widget.controller == TextEditingController(),
-        // widget.onChanged == (value) {},
+            widget.prefixIcon == Icons.account_circle,
       ),
       findsOneWidget,
     );
