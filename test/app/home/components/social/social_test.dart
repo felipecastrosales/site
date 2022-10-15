25aae08e51395c65c06f78912ae2f1f44d53809b
Felipe Sales <soufeliposales@gmail.com>
2022-10-15 10:40:40 -0300

    test: add Social widgets test

---
+++ test/app/home/components/social/social_test.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:site/app/home/components/social/social.dart';
+
+void main() {
+  testWidgets('Should renders Social', (tester) async {
+    await _createWidget(tester: tester);
+
+    expect(
+      find.byType(Social),
+      findsOneWidget,
+    );
+  });
+}
+
+Future<void> _createWidget({
+  required WidgetTester tester,
+}) async {
+  await tester.pumpWidget(
+    const MaterialApp(
+      home: Social(),
+    ),
+  );
+}
