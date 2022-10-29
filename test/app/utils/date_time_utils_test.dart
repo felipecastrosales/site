4dd94195e92978d70486cdd36b06d5ae7e78b63b
Felipe Sales <soufeliposales@gmail.com>
2022-10-29 11:01:54 -0300

    fix: fix test type

--- test/app/utils/date_time_utils_test.dart
+++ test/app/utils/date_time_utils_test.dart
@@ -2,7 +2,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:site/app/utils/utils.dart';
 
 void main() {
-  testWidgets('DateTimeUtils get year', (tester) async {
+  test('DateTimeUtils get year', () async {
     var date = DateTimeUtils.getYear(DateTime(2003));
     expect(date, isNotNull);
   });
--- test/app/utils/launch_urls_test.dart
+++ test/app/utils/launch_urls_test.dart
@@ -1,9 +1,14 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:site/app/utils/utils.dart';
 
 void main() {
-  testWidgets('LaunchUrls launch Url', (tester) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  test('LaunchUrls launch Url', () async {
     var url = LaunchUrls.launchURL('https://github.com/felipecastrosales');
-    expect(() => url, isNotNull);
+    expect(
+      () => url,
+      isNotNull,
+    );
   });
 }
